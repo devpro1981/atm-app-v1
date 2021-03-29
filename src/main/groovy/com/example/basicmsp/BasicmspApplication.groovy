f75aff58b20bb866952849e14949fd61363745ad
Salim Ali <msalima@lexmark.com>
2021-03-29 20:31:23 +0530

    Project setup from Spring Initializr

---
+++ src/main/groovy/com/example/basicmsp/BasicmspApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.basicmsp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BasicmspApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BasicmspApplication, args)
+	}
+
+}
