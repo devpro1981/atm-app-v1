c6926da3e084479d3532fd6e273804a485297409
msalima <salim.ali@lexmark.com>
2022-06-26 20:17:47 +0530

    Init project

---
+++ src/test/groovy/com/zinksoft/atmApp/AtmAppApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.zinksoft.atmApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AtmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
