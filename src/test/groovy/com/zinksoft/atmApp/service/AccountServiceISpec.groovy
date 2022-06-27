e4a527ea8c703dc87370a0b6285366088df4e6ab
msalima <salim.ali@lexmark.com>
2022-06-27 20:20:04 +0530

    More integration tests

--- src/test/groovy/com/zinksoft/atmApp/service/AccountServiceISpec.groovy
+++ src/test/groovy/com/zinksoft/atmApp/service/AccountServiceISpec.groovy
@@ -16,8 +16,11 @@ class AccountServiceISpec extends BaseIntegrationSpec {
     @Autowired
     EntityManager entityManager
 
-    def setup() {
+    Account userAccount
 
+    def setup() {
+        userAccount = accountService.createAccount('12345', '6543', 1500, 500)
+        entityManager.flush()
     }
 
     def cleanup () {
@@ -26,9 +29,6 @@ class AccountServiceISpec extends BaseIntegrationSpec {
 
     @Transactional
     def 'Account retrieval'() {
-        setup:
-        Account userAccount = accountService.createAccount('12345', '6543', 1500, 500)
-        entityManager.flush()
 
         when:
         Account queriedAccount = accountService.retrieveAccount('12345')
@@ -39,5 +39,28 @@ class AccountServiceISpec extends BaseIntegrationSpec {
         accountService.calculateAvailableBalance(queriedAccount).maxAvailableBalance
                 == userAccount.accountBalances.currentBalance + userAccount.accountBalances.overdraft
 
+    }
+
+    @Transactional
+    def 'Account balance handling'() {
+
+        when: 'withdrawal amount is less than current balance'
+        Account queriedAccount = accountService.retrieveAccount('12345')
+        Account updatedAccount = accountService.debitAccount(queriedAccount, 100)
+        entityManager.flush()
+
+        then: 'overdraft amount is left untouched'
+        updatedAccount.accountBalances.currentBalance == queriedAccount.accountBalances.openingBalance - 100
+        updatedAccount.accountBalances.overdraft == queriedAccount.accountBalances.overdraft
+
+        when: 'withdrawal amount is more than current balance'
+        queriedAccount = accountService.retrieveAccount('12345')
+        Integer overdraftBeforeDebit = queriedAccount.accountBalances.overdraft
+        updatedAccount = accountService.debitAccount(queriedAccount, 1600)
+        entityManager.flush()
+
+        then: 'overdraft amount is reduced'
+        updatedAccount.accountBalances.currentBalance == 0
+        updatedAccount.accountBalances.overdraft == overdraftBeforeDebit - 200
     }
 }
